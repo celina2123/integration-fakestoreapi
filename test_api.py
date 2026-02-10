5184747208372dab1053f32f1b1f0027d03dc0fc
celina2123 <celinaelheddad@gmail.com>
2026-02-10 21:04:49 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,12 +1,5 @@
 import requests
 
 def test_get_products_returns_200():
-    url = "https://fakestoreapi.com/products"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
-    response = requests.get(url, headers=headers)
-
-    assert response.status_code == 200
+    r = requests.get("https://fakestoreapi.com/products")
+    assert r.status_code == 200
